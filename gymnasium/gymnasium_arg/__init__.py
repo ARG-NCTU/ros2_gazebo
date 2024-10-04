0056ab8bfb15e980583a31e11e4e7f916cb54780
uwe77 <uwe90711@gmail.com>
2024-10-04 14:19:12 +0800

    feat gymnasium for training

---
+++ gymnasium/gymnasium_arg/__init__.py
@@ -0,0 +1,111 @@
+# from gymnasium.envs.registration import make, register, registry, spec
+
+
+# register(
+#     id="wamv-motor-anchoring-v6",
+#     entry_point="gymnasium_vrx.envs:WamvMotorAnchoringV6",
+#     max_episode_steps=4096,
+# )
+
+# register(
+#     id="wamv-motor-anchoring-v4",
+#     entry_point="gymnasium_vrx.envs:WamvMotorAnchoringV4",
+#     max_episode_steps=4096,
+# )
+
+
+# register(
+#     id="wamv-motor-anchoring-v3",
+#     entry_point="gymnasium_vrx.envs:WamvMotorAnchoringV3",
+#     max_episode_steps=4096,
+# )
+
+# register(
+#     id="wamv-motor-anchoring-v2",
+#     entry_point="gymnasium_vrx.envs:WamvMotorAnchoringV2",
+#     max_episode_steps=4096,
+# )
+
+# register(
+#     id="drone-range-track-v1",
+#     entry_point="gymnasium_vrx.envs:DroneRangeTrackV1",
+#     max_episode_steps=128,
+# )
+
+# register(
+#     id="drone-range-track-v2",
+#     entry_point="gymnasium_vrx.envs:DroneRangeTrackV2",
+#     max_episode_steps=128,
+# )
+
+# register(
+#     id="drone-range-track-v3",
+#     entry_point="gymnasium_vrx.envs:DroneRangeTrackV3",
+#     max_episode_steps=128,
+# )
+
+# register(
+#     id="drone-range-track-v4",
+#     entry_point="gymnasium_vrx.envs:DroneRangeTrackV4",
+#     max_episode_steps=128,
+# )
+
+# register(
+#     id="drone-range-land-v1",
+#     entry_point="gymnasium_vrx.envs:DroneRangeLandV1",
+#     max_episode_steps=128,
+# )
+
+# register(
+#     id="drone-range-land-v2",
+#     entry_point="gymnasium_vrx.envs:DroneRangeLandV1",
+#     max_episode_steps=128,
+# )
+
+# register(
+#     id="drone-range-land-v3",
+#     entry_point="gymnasium_vrx.envs:DroneRangeLandV3",
+#     max_episode_steps=128,
+# )
+
+# register(
+#     id="drone-range-land-v3",
+#     entry_point="gymnasium_vrx.envs:DroneRangeLandV4",
+#     max_episode_steps=128,
+# )
+
+# # register(
+# #     id="uav-lander-v1",
+# #     entry_point="gymnasium_vrx.envs:UavLanderV1",
+# #     max_episode_steps=1024,
+# # )
+
+# # register(
+# #     id="uav-lander-v2",
+# #     entry_point="gymnasium_vrx.envs:UavLanderV2",
+# #     max_episode_steps=1024,
+# # )
+
+# # register(
+# #     id="uav-lander-v3",
+# #     entry_point="gymnasium_vrx.envs:UavLanderV3",
+# #     max_episode_steps=1024,
+# # )
+
+# # register(
+# #     id="uav-lander-v4",
+# #     entry_point="gymnasium_vrx.envs:UavLanderV4",
+# #     max_episode_steps=1024,
+# # )
+
+# register(
+#     id="uav-lander-v5",
+#     entry_point="gymnasium_vrx.envs:UavLanderV5",
+#     max_episode_steps=1024,
+# )
+
+# register(
+#     id="vrx-goal-v0",
+#     entry_point="gymnasium_vrx.envs:VRXGoalV0",
+#     max_episode_steps=4096,
+# )
---
+++ gymnasium/gymnasium_arg/envs/__init__.py
@@ -0,0 +1,22 @@
+# from gymnasium_vrx.envs.drone_range_land_v1 import DroneRangeLandV1
+# from gymnasium_vrx.envs.drone_range_land_v2 import DroneRangeLandV2
+# from gymnasium_vrx.envs.drone_range_land_v3 import DroneRangeLandV3
+# from gymnasium_vrx.envs.drone_range_land_v4 import DroneRangeLandV4
+# from gymnasium_vrx.envs.drone_range_track_v1 import DroneRangeTrackV1
+# from gymnasium_vrx.envs.drone_range_track_v2 import DroneRangeTrackV2
+# from gymnasium_vrx.envs.drone_range_track_v3 import DroneRangeTrackV3
+# from gymnasium_vrx.envs.drone_range_track_v4 import DroneRangeTrackV4
+
+# try:
+#     # from gymnasium_vrx.envs.uavlander_v1 import UavLanderV1
+#     # from gymnasium_vrx.envs.uavlander_v2 import UavLanderV2
+#     # from gymnasium_vrx.envs.uavlander_v3 import UavLanderV3
+#     # from gymnasium_vrx.envs.uavlander_v4 import UavLanderV4
+#     from gymnasium_vrx.envs.uavlander_v5 import UavLanderV5
+#     from gymnasium_vrx.envs.vrx_goal_v0 import VRXGoalV0
+#     from gymnasium_vrx.envs.vrx_motor_anchoring_v2 import WamvMotorAnchoringV2
+#     from gymnasium_vrx.envs.vrx_motor_anchoring_v3 import WamvMotorAnchoringV3
+#     from gymnasium_vrx.envs.vrx_motor_anchoring_v4 import WamvMotorAnchoringV4
+#     from gymnasium_vrx.envs.vrx_motor_anchoring_v6 import WamvMotorAnchoringV6
+# except ImportError:
+#     pass
