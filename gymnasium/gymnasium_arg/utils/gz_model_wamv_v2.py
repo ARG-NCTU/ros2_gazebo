31c52b4a457c14c72ec76ee1a774c49dc817b270
uwe77 <uwe90711@gmail.com>
2024-11-12 22:29:38 +0800

    Update WAMV models for velocity constraints, pose history, and topic subscription
    - Updated `gz_model_wamv_v1.py` and `gz_model_wamv_v2.py` to include maximum linear (3 m/s) and angular (1.25 rad/s) velocity constraints.
    - Added `PoseArray` subscription to handle WAMV position and orientation data, rolling the pose history for each time step.
    - Implemented pose history maintenance with rolling arrays for enhanced observation consistency across historical frames.
    - Minor corrections and improvements in type consistency and logging comments for clarity in sensor subscription initialization.

--- gymnasium/gymnasium_arg/utils/gz_model_wamv_v2.py
+++ gymnasium/gymnasium_arg/utils/gz_model_wamv_v2.py
@@ -9,7 +9,7 @@ import launch
 from scipy.spatial.transform import Rotation as R
 from rclpy.node import Node
 from rosgraph_msgs.msg import Clock
-from geometry_msgs.msg import TwistStamped, Pose, Twist, Point, Quaternion, PoseStamped
+from geometry_msgs.msg import TwistStamped, Pose, Twist, Point, Quaternion, PoseStamped, PoseArray
 from sensor_msgs.msg import Imu
 from ros_gz_interfaces.srv import DeleteEntity, SpawnEntity
 from std_msgs.msg import Float32, Float64
@@ -18,17 +18,20 @@ import xml.etree.ElementTree as ET
 from gymnasium_arg.utils.gz_model import GZ_MODEL
 
 
-class WAMVV2_GZ_MODEL(GZ_MODEL):
+class WAMVV1_GZ_MODEL(GZ_MODEL):
 
         
     def __init__(self, world, name, path, pose: Pose, info={'veh':'wamv_v2', 'maxstep': 4096, 'max_thrust': 15*746/9.8, 'hist_frame': 5}):
         super().__init__(orig_name=info['veh'], name=name, path=path, world=world, init_pose=pose)
         self.info['maxstep'] = info['maxstep']
         self.info['max_thrust'] = info['max_thrust']
+        self.info['max_lin_velocity'] = 3.0 # 3 m/s
+        self.info['max_ang_velocity'] = 1.25 # 1 rad/s
         self.info['hist_frame'] = info['hist_frame']
         self.info['step_cnt'] = 0
         self.sub['imu'] = self.create_subscription(Imu, f"/world/{world}/model/{name}/link/imu_link/sensor/imu_sensor/imu", self.__imu_cb, 10)
         self.sub['termination'] = self.create_subscription(Float64, f"/world/{world}/model/{name}/link/base_link/sensor/sensor_contact/contact", self.__termination_cb, 1)
+        self.sub['pose'] = self.create_subscription(PoseArray, f"/model/{name}/pose", self.__pose_cb, 10)
         self.pub['cmd_vel'] = self.create_publisher(TwistStamped, f'/{name}/cmd_vel', 1)
 
         self.launch_service = LaunchService()
@@ -69,9 +72,9 @@ class WAMVV2_GZ_MODEL(GZ_MODEL):
 
         # self.obs['action'] = Twist()
         self.obs['action'] = np.zeros((self.info['hist_frame'], 6))
-        # self.obs['pose'] = Pose()
         self.obs['imu'] = np.array([])
-        
+        self.obs['pose'] = np.array([], dtype=np.float32)
+        # self.obs['last_pose'] = self.obs['pose']
         self.obs['termination'] = False
         self.obs['truncation'] = False
 
@@ -82,26 +85,32 @@ class WAMVV2_GZ_MODEL(GZ_MODEL):
         while self.obs['imu'].shape != (self.info['hist_frame'], 10):
             # print(f"Waiting for {self.info['name']} imu...")
             pass
+        while self.obs['pose'].shape != (self.info['hist_frame'], 7):
+            # print(f"Waiting for {self.info['name']} pose...")
+            pass
         return self.obs
     
     def reset(self):
         super().reset()
         self.obs['action'] = np.zeros((self.info['hist_frame'], 6))
         self.obs['imu'] = np.array([])
+        self.obs['pose'] = np.array([], dtype=np.float32)
+        # self.obs['last_pose'] = self.obs['pose']
         self.obs['termination'] = False
         self.obs['truncation'] = False
         self.info['step_cnt'] = 0
 
     def step(self, action: TwistStamped):
         self.obs['action'] = np.roll(self.obs['action'], 1, axis=0)
-        self.obs['action'][0] = np.array(
-            [action.twist.linear.x, action.twist.linear.y, action.twist.linear.z, 
-             action.twist.angular.x, action.twist.angular.y, action.twist.angular.z]
-        )
+        self.obs['action'][0] = np.array([
+            action.twist.linear.x, action.twist.linear.y, action.twist.linear.z, 
+            action.twist.angular.x, action.twist.angular.y, action.twist.angular.z
+            ], dtype=np.float32)
         self.info['step_cnt'] += 1
         self.pub['cmd_vel'].publish(action)
         if self.info['step_cnt'] >= self.info['maxstep']:  # Corrected: self.step_cnt -> self.info['step_cnt']
             self.obs['truncation'] = True
+        # self.obs['last_pose'] = self.obs['pose']
     
     def close(self):
         self.get_logger().info("Closing the service...")
@@ -127,3 +136,17 @@ class WAMVV2_GZ_MODEL(GZ_MODEL):
 
     def __termination_cb(self, msg):
         self.obs['termination'] = True if msg is not None else False
+
+    def __pose_cb(self, msg):
+        pose = np.array([
+            msg.poses[0].position.x, msg.poses[0].position.y, msg.poses[0].position.z,
+            msg.poses[0].orientation.x, msg.poses[0].orientation.y, msg.poses[0].orientation.z, msg.poses[0].orientation.w
+        ], dtype=np.float32)
+        if self.obs['pose'].shape != (self.info['hist_frame'], 7):
+            if self.obs['pose'].shape == (0,):
+                self.obs['pose'] = pose
+            else:
+                self.obs['pose'] = np.vstack((pose, self.obs['pose']))
+        else:
+            self.obs['pose'] = np.roll(self.obs['pose'], 1, axis=0)
+            self.obs['pose'][0] = pose
