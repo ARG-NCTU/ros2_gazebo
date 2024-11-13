37d3bbafcaeaf5738acec11e6387c61f6bfa42e2
uwe77 <uwe90711@gmail.com>
2024-11-13 10:37:42 +0800

    Fix Import Errors for USV Environment and WAMV Models
    - Added `USV_V1` to `__init__.py` in `envs` for proper environment registration.
    - Corrected `WAMVV2_GZ_MODEL` import in `utils/__init__.py` to fix import duplication.

--- gymnasium/gymnasium_arg/envs/__init__.py
+++ gymnasium/gymnasium_arg/envs/__init__.py
@@ -2,3 +2,4 @@ from gymnasium_arg.envs.multi_blueboat_v1 import Multi_BlueBoat_V1
 from gymnasium_arg.envs.blueboat_v1 import BlueBoat_V1
 from gymnasium_arg.envs.blueboat_v2 import BlueBoat_V2
 from gymnasium_arg.envs.blueboat_v3 import BlueBoat_V3
+from gymnasium_arg.envs.usv_v1 import USV_V1
--- gymnasium/gymnasium_arg/utils/__init__.py
+++ gymnasium/gymnasium_arg/utils/__init__.py
@@ -1,4 +1,4 @@
 from gymnasium_arg.utils.gz_model import BlueBoat_GZ_MODEL
 from gymnasium_arg.utils.gz_model_wamv_v1 import WAMVV1_GZ_MODEL
-from gymnasium_arg.utils.gz_model_wamv_v2 import WAMVV1_GZ_MODEL
+from gymnasium_arg.utils.gz_model_wamv_v2 import WAMVV2_GZ_MODEL
 from gymnasium_arg.utils.vae import VAE
