99f7f24d5f4758f92b975a183de80144d176287a
Laipi1240 <charles92421@gmail.com>
2025-03-18 19:14:44 +0800

    update start_ardu_sim.sh for commanded ardupilot param setup

--- start_ardu_sim.sh
+++ start_ardu_sim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
+sim_vehicle.py -v Rover --map --console --out=udp:192.168.0.110:14551 --out=udp:192.168.0.110:14550 -l 22.59920,120.298691,0,270
 
-sim_vehicle.py -v Rover --map --console --out=udp:192.168.0.110:14551 --out=udp:192.168.0.110:14550 --custom-location=22.59920,120.28832,0,270
-
-
+# param sett FRAME_CLASS 2
+# param set FRAME_TYPE 0
