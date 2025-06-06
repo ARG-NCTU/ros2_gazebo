70e16f1c4ab0fc731b7d5b3f445d848bf90c0a7d
Laipi1240 <charles92421@gmail.com>
2025-06-06 17:06:24 +0800

    add boat.parm file

--- start_ardu_sim.sh
+++ start_ardu_sim.sh
@@ -2,8 +2,8 @@
 
 mkdir -p ./sim_log && cd ./sim_log || exit 1
 
-sim_vehicle.py -v Rover --add-param-file=../boat.parm --map --console --out=udp:192.168.0.110:14551 --out=udp:192.168.0.110:14550 -l 22.59920,120.298691,0,270
+sim_vehicle.py -v Rover --add-param-file=../boat.parm --console --out=udp:192.168.0.110:14551 --out=udp:192.168.0.110:14550 -l 22.59920,120.298691,0,270
 
 cd ..
-# param sett FRAME_CLASS 2
+# param set FRAME_CLASS 2
 # param set FRAME_TYPE 0
